d794d937e457af01d7eebbf498d87fa322f7e90c
Joshua Hoblitt <josh@hoblitt.com>
2025-05-12 16:32:56 -0700

    rm s3daemon::instance::port param
    This param creates an env var in the container and the hardcoded
    behavior prevents the env var from being renamed by the implementation.

--- examples/instances.pp
+++ examples/instances.pp
@@ -1,28 +1,26 @@
 class { 's3daemon':
   env       => {
-    'QUUX' => 'corge',
+    'S3DAEMON_PORT' => 15556,
+    'QUUX'          => 'corge',
   },
   instances => {
     'foo' => {
       'aws_access_key_id'     => 'access_key_id',
       'aws_secret_access_key' => 'secret_access_key',
-      'port'                  => 15556,
       'image'                 => 'ghcr.io/lsst-dm/s3daemon:main',
       'env'                   => {
         'S3_ENDPOINT_URL' => 'https://s3.foo.example.com',
         'FOO'             => 'bar',
-        'BAZ'             => 'qux',
       },
     },
     'bar' => {
       'aws_access_key_id'     => 'access_key_id',
       'aws_secret_access_key' => 'secret_access_key',
-      'port'                  => 15557,
       'image'                 => 'ghcr.io/lsst-dm/s3daemon:sha-b5e72fa',
       'volumes'               => ['/home:/home', '/opt:/opt'],
       'env'                   => {
         'S3_ENDPOINT_URL' => 'https://s3.bar.example.com',
-        'FOO'             => 'bar',
+        'S3DAEMON_PORT'   => 15557,
         'BAZ'             => 'qux',
       },
     },
